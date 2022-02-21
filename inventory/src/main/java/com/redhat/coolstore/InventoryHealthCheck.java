17b40a14a4e14b197495af792f3ea3d6598ceb7c
Manuel Hahn <manuel.hahn@gmx.de>
2022-02-21 16:39:03 +0100

    Commented health checks (shall be activated during demo).

--- inventory/src/main/java/com/redhat/coolstore/InventoryHealthCheck.java
+++ inventory/src/main/java/com/redhat/coolstore/InventoryHealthCheck.java
@@ -1,26 +1,26 @@
-package com.redhat.coolstore;
+// package com.redhat.coolstore;
 
-import javax.enterprise.context.ApplicationScoped;
-import javax.inject.Inject;
+// import javax.enterprise.context.ApplicationScoped;
+// import javax.inject.Inject;
 
-import org.eclipse.microprofile.health.HealthCheck;
-import org.eclipse.microprofile.health.HealthCheckResponse;
-import org.eclipse.microprofile.health.Readiness;
+// import org.eclipse.microprofile.health.HealthCheck;
+// import org.eclipse.microprofile.health.HealthCheckResponse;
+// import org.eclipse.microprofile.health.Readiness;
 
-@Readiness
-@ApplicationScoped
-public class InventoryHealthCheck implements HealthCheck {
+// @Readiness
+// @ApplicationScoped
+// public class InventoryHealthCheck implements HealthCheck {
 
-    @Inject
-    private InventoryResource inventoryResource;
+//     @Inject
+//     private InventoryResource inventoryResource;
 
-    @Override
-    public HealthCheckResponse call() {
+//     @Override
+//     public HealthCheckResponse call() {
 
-        if (inventoryResource.getAll() != null) {
-            return HealthCheckResponse.named("Success of Inventory Health Check!!!").up().build();
-        } else {
-            return HealthCheckResponse.named("Failure of Inventory Health Check!!!").down().build();
-        }
-    }
-}
+//         if (inventoryResource.getAll() != null) {
+//             return HealthCheckResponse.named("Success of Inventory Health Check!!!").up().build();
+//         } else {
+//             return HealthCheckResponse.named("Failure of Inventory Health Check!!!").down().build();
+//         }
+//     }
+// }
